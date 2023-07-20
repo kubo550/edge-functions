9236cb9372130c0944eb9ec65ddc0e2f231b0f64
jakubkurdziel <jakub.kurdziel@talkie.ai>
2023-07-20 11:42:08 +0200

    create stack-overflow stats image

--- supabase/functions/stack-stats/handler.tsx
+++ supabase/functions/stack-stats/handler.tsx
@@ -2,6 +2,16 @@ import React from "https://esm.sh/react@18.2.0";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 import {ImageResponse} from 'https://deno.land/x/og_edge/mod.ts'
 
+
+enum Color {
+    BACKGROUND = '#2D2D2D',
+    TEXT = '#C4C8CC',
+    GOLD = '#F0B400',
+    SILVER = '#999C9F',
+    BRONZE = '#AB825F',
+}
+
+
 export default async function handler(req: Request) {
 
     if (req.method !== "GET") {
@@ -21,49 +31,65 @@ export default async function handler(req: Request) {
         )
     }
 
-    const {reputation, profile_image, badge_counts} = data.items[0]
+    const {reputation, profile_image, badge_counts} = data.items[0];
 
-    console.log({reputation, profile_image, badge_counts})
+    const getBadgeStyle = (color: Color) => ({
+        width: 6,
+        height: 6,
+        borderRadius: '50%',
+        overflow: 'hidden',
+        marginRight: 2,
+        backgroundColor: color,
+    });
+
+    const getBadgeContainerStyle = (color: Color) => ({
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        marginLeft: 8,
+        color: color,
+    });
+
+    const containerStyle = {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: 12,
+        backgroundColor: Color.BACKGROUND,
+        height: 52,
+        padding: '0 8px',
+    };
+    const imageStyle = {
+        width: 24,
+        height: 24,
+        objectFit: 'cover',
+        marginRight: 8,
+    } as const;
 
     return new ImageResponse(
-        <div
-            style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: 12,
-                backgroundColor: '#2D2D2D',
-                height: 52,
-                padding: '0 8px',
+        <div style={containerStyle}>
 
-            }}
-        >
+            <img src={profile_image} alt='profile' style={imageStyle}/>
 
-                <span style={{color: '#C4C8CC'}}>
-                    3,077
-                </span>
-
-            <span style={{display: "flex",
-                alignItems: "center",
-                justifyContent: "center",}}>
-                    <span style={{
-                        width: 4,
-                        height: 4,
-                        borderRadius: '50%',
-                        overflow: 'hidden',
-                        backgroundColor: '#fff',
-                    }}/>
-                    12
-                </span>
-
-            <span title="12 silver badges" aria-hidden="true">
+            <span style={{color: Color.TEXT}}>
+               {reputation}
             </span>
-            <span title="22 bronze badges" aria-hidden="true">
+
+            <span style={getBadgeContainerStyle(Color.GOLD)}>
+                <span style={getBadgeStyle(Color.GOLD)}/>
+                {badge_counts.gold}
+            </span>
+
+            <span style={getBadgeContainerStyle(Color.SILVER)}>
+                <span style={getBadgeStyle(Color.SILVER)}/>
+                {badge_counts.silver}
+            </span>
+
+            <span style={getBadgeContainerStyle(Color.BRONZE)}>
+                <span style={getBadgeStyle(Color.BRONZE)}/>
+                {badge_counts.bronze}
             </span>
 
         </div>
-    )
-        ;
-
-
+    );
 }
